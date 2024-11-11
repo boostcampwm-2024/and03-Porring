97bf82da8b9f100cb0040dc4277bc0a42c0a0c14
Inwoo <lys91892@gmail.com>
2024-11-11 18:02:40 +0900

    UI : FavoriteGroup Component - #13

--- feature/home/src/main/java/com/kolown/home/HomeScreen.kt
+++ feature/home/src/main/java/com/kolown/home/HomeScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.porring.home.component.FavoriteGroup
 import com.porring.home.component.RandomImageList
 
 @Composable
