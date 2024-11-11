6fc0c50afc626fe00b1c28646406c402001c94c5
Inwoo <lys91892@gmail.com>
2024-11-11 22:47:04 +0900

    File : IconLove & ReactionGroup Rename - #13

--- feature/home/src/main/java/com/porring/home/component/ReactionGroup.kt
+++ feature/home/src/main/java/com/porring/home/component/ReactionGroup.kt
@@ -1,6 +1,5 @@
 package com.porring.home.component
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.offset
@@ -13,12 +12,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 
 @Composable
-internal fun FavoriteGroup(
+internal fun ReactionGroup(
     modifier: Modifier,
     reactions: List<Int>
 ) {
