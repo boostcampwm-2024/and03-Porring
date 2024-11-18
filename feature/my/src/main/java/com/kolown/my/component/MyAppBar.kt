b634d9563dd044e96a77c0dededfc30afa0624a3
Moony-H <53536205+Moony-H@users.noreply.github.com>
2024-11-19 01:34:25 +0900

    Fix: Scroll Bug Fix

---
+++ feature/my/src/main/java/com/kolown/my/component/MyAppBar.kt
@@ -0,0 +1,48 @@
+package com.kolown.my.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun MyAppBar(onSettingClicked: () -> Unit = {}) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .background(Color.White)
+    ) {
+        Text(
+            "My Gallery",
+            modifier = Modifier.align(Alignment.Center),
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        IconButton(onClick = onSettingClicked, modifier = Modifier.align(Alignment.CenterEnd)) {
+            Icon(Icons.Filled.Settings, contentDescription = null)
+        }
+
+    }
+}
+
+@Composable
+@Preview
+fun MyAppBarPreview() {
+    MyAppBar()
+}
