3ce4b7ad8ce7f323cd807d3c2a558f0f505442d8
Moony-H <53536205+Moony-H@users.noreply.github.com>
2024-11-19 16:25:56 +0900

    Fix: dataSource field 이름 변경

--- core/data/src/main/java/com/kolown/data/repository/GalleryRepository.kt
+++ core/data/src/main/java/com/kolown/data/repository/GalleryRepository.kt
@@ -3,7 +3,6 @@ package com.kolown.data.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import com.kolown.data.datasource.FakeGalleryDataSource
 import com.kolown.data.datasource.GalleryDataSource
 import com.kolown.data.datasource.GalleryPagingDataSource
 import com.kolown.model.Gallery
@@ -19,7 +18,7 @@ interface GalleryRepository {
 }
 
 class GalleryRepositoryImpl @Inject constructor(
-    private val dataSourceFake: GalleryDataSource,
+    private val dataSource: GalleryDataSource,
 ) : GalleryRepository {
     override suspend fun getGalleryThumbnailList(
         userId: Long,
@@ -28,20 +27,20 @@ class GalleryRepositoryImpl @Inject constructor(
 
         if (Random.nextInt(3) == 0)
             return Result.failure(Exception("테스트 에러 발생"))
-        return dataSourceFake.getGalleryThumbnailList(userId, page)
+        return dataSource.getGalleryThumbnailList(userId, page)
     }
 
     override suspend fun getGallery(id: Long): Result<Gallery> {
         if (Random.nextInt(3) == 0)
             return Result.failure(Exception("테스트 에러 발생"))
-        return dataSourceFake.getGallery(id)
+        return dataSource.getGallery(id)
     }
 
     override fun getGalleryThumbnailPagingFlow(userId: Long): Flow<PagingData<GalleryThumbnail>> {
         return Pager(config = PagingConfig(
             pageSize = PAGE_SIZE,
             enablePlaceholders = false,
-        ), pagingSourceFactory = { GalleryPagingDataSource(dataSourceFake, userId) }).flow
+        ), pagingSourceFactory = { GalleryPagingDataSource(dataSource, userId) }).flow
     }
 
     companion object {
