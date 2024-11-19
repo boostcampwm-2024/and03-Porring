d0541f6452fb82f3da20b5ea2cacd294adb6a774
HahyunKang <ganghahyun06@gmail.com>
2024-11-19 22:45:21 +0900

    Feat: exception handling

---
+++ feature/detail/src/main/java/com/kolown/detail/LoadingDetailScreen.kt
@@ -0,0 +1,115 @@
+package com.kolown.detail
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsIgnoringVisibility
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+import com.kolown.detail.component.FollowDialog
+import com.kolown.detail.component.ReactionDialog
+import com.kolown.detail.component.shimmerEffect
+import com.kolown.model.ImageItem
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
+@Composable
+fun LoadingDetailScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                windowInsets = WindowInsets.statusBarsIgnoringVisibility,
+                title = {},
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
+                        contentDescription = "뒤로 가기",
+                        tint = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Gray)
+            )
+        },
+        containerColor = Color.Gray
+    ) { padding ->
+        LoadingDetailContent(padding = padding)
+    }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun LoadingDetailContent(
+    padding: PaddingValues,
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(4f / 5f)
+                .shimmerEffect()
+        )
+        Spacer(Modifier.height(10.dp))
+        Box(
+            modifier = Modifier
+                .padding(start = 10.dp)
+                .fillMaxWidth(0.7f)
+                .height(20.dp)
+                .shimmerEffect(),
+        )
+        Spacer(Modifier.height(10.dp))
+
+        Box(
+            modifier = Modifier
+                .padding(start = 10.dp)
+                .fillMaxWidth(0.5f)
+                .height(20.dp)
+                .shimmerEffect(),
+        )
+
+    }
+}
+
