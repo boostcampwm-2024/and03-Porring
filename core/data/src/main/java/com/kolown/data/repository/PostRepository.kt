6bc735d2a125b2f87253238b4caf19c2d5a99955
Inwoo <lys91892@gmail.com>
2024-11-20 16:45:55 +0900

    Feat : add TagDataSource - #27
    Co-authored-by: bong-pal <etgt777@naver.com>

--- core/data/src/main/java/com/kolown/data/repository/PostRepository.kt
+++ core/data/src/main/java/com/kolown/data/repository/PostRepository.kt
@@ -1,22 +1,25 @@
 package com.kolown.data.repository
 
+import android.net.Uri
 import com.kolown.data.datasource.ImageDataSource
 import com.kolown.data.datasource.PostDataSource
+import com.kolown.data.datasource.TagDatasource
 import java.io.IOException
 import javax.inject.Inject
 
 interface PostRepository {
-    suspend fun uploadPost(fileUri: String, description: String, tags: List<String>): Result<Unit>
+    suspend fun uploadPost(fileUri: Uri, description: String, tags: List<String>): Result<Unit>
 }
 
 class PostRepositoryImpl @Inject constructor(
     private val imageDataSource: ImageDataSource,
     private val postDataSource: PostDataSource,
+    private val tagDataSource: TagDatasource
 ) : PostRepository {
     private val authorId = "user-1feIeEN3rMh4ZY7YpQKxDfnKGvi2"
 
     override suspend fun uploadPost(
-        fileUri: String,
+        fileUri: Uri,
         description: String,
         tags: List<String>
     ): Result<Unit> {
@@ -29,6 +32,7 @@ class PostRepositoryImpl @Inject constructor(
             val postId = postDataSource.uploadPost(authorId, imageUri, description).getOrElse {
                 throw IOException("게시물 업로드 실패")
             }
+            tagDataSource.uploadTags(tags, postId)
         }
     }
 
