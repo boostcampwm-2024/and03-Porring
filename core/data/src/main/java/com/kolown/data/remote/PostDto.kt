d4b3ed2e9ff8fa9e1f934e17e9cee0008ba6dc47
Inwoo <lys91892@gmail.com>
2024-11-20 16:34:40 +0900

    Feat : post Upload to Firestore & get postId - #27
    Co-authored-by: bong-pal <etgt777@naver.com>

---
+++ core/data/src/main/java/com/kolown/data/remote/PostDto.kt
@@ -0,0 +1,35 @@
+package com.kolown.data.remote
+
+import com.kolown.data.randomValue
+import com.kolown.model.PostModel
+
+data class PostDto(
+    val postId: String = "",
+    val authorId: String = "",
+    val imageUrl: String = "",
+    val registerAt: String = "",
+    val description: String = "",
+    val randomA: Long = randomValue(),
+    val randomB: Long = randomValue(),
+    val randomC: Long = randomValue(),
+    val randomD: Long = randomValue(),
+    val randomE: Long = randomValue(),
+)
+
+fun PostDto.toPostModel(seed: String = "A"): PostModel {
+    return PostModel(
+        postId = this.postId,
+        authorId = this.authorId,
+        imageUrl = this.imageUrl,
+        registerAt = this.registerAt,
+        description = this.description,
+        random = when (seed) {
+            "A" -> randomA
+            "B" -> randomB
+            "C" -> randomC
+            "D" -> randomD
+            "E" -> randomE
+            else -> throw IllegalArgumentException("존재하지 않는 랜덤 시드")
+        }
+    )
+}
--- core/data/src/main/java/com/kolown/data/repository/PostRepository.kt
+++ core/data/src/main/java/com/kolown/data/repository/PostRepository.kt
@@ -1,6 +1,7 @@
 package com.kolown.data.repository
 
 import com.kolown.data.datasource.ImageDataSource
+import com.kolown.data.datasource.PostDataSource
 import javax.inject.Inject
 
 interface PostRepository {
@@ -9,6 +10,7 @@ interface PostRepository {
 
 class PostRepositoryImpl @Inject constructor(
     private val imageDataSource: ImageDataSource,
+    private val postDataSource: PostDataSource,
 ) : PostRepository {
     private val authorId = "user-1feIeEN3rMh4ZY7YpQKxDfnKGvi2"
 
@@ -18,8 +20,10 @@ class PostRepositoryImpl @Inject constructor(
         tags: List<String>
     ): Result<Unit> {
         return runCatching {
+            // Image File Upload & get Image Uri
             val imageUri = imageDataSource.getImageUrl(authorId, fileUri).getOrThrow()
-
+            // post Upload to Firestore & get postId
+            val postId = postDataSource.uploadPost(authorId, imageUri, description).getOrThrow()
         }
     }
 
