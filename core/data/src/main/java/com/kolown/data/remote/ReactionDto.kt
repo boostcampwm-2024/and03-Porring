14322f600b4b79fc06ae59c60fd1389aba6f66ae
Inwoo <lys91892@gmail.com>
2024-11-21 00:19:11 +0900

    Feat : ReactionDataSource 생성 - #37
    Co-authored-by: bong-pal <etgt777@naver.com>
    Co-authored-by: HahyunKang <ganghahyun06@gmail.com>

---
+++ core/data/src/main/java/com/kolown/data/remote/ReactionDto.kt
@@ -0,0 +1,17 @@
+package com.kolown.data.remote
+
+import com.kolown.model.ReactionModel
+import com.kolown.model.toReactions
+
+data class ReactionDto(
+    val reactionId: String = "",
+    val userId: String = "",
+    val postId: String = "",
+    val reaction: Int? = null
+)
+
+fun ReactionDto.toReactionModel() = ReactionModel(
+    userId = userId,
+    postId = postId,
+    reaction = reaction?.toReactions()
+)
--- core/data/src/main/java/com/kolown/data/repository/PostRepository.kt
+++ core/data/src/main/java/com/kolown/data/repository/PostRepository.kt
@@ -4,11 +4,13 @@ import android.net.Uri
 import android.util.Log
 import com.kolown.data.datasource.remote.ImageDataSource
 import com.kolown.data.datasource.remote.PostDataSource
+import com.kolown.data.datasource.remote.ReactionDataSource
 import com.kolown.data.datasource.remote.TagDatasource
 import com.kolown.model.PostContentModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import java.io.IOException
 import javax.inject.Inject
@@ -21,7 +23,8 @@ interface PostRepository {
 class PostRepositoryImpl @Inject constructor(
     private val imageDataSource: ImageDataSource,
     private val postDataSource: PostDataSource,
-    private val tagDataSource: TagDatasource
+    private val tagDataSource: TagDatasource,
+    private val reactionDataSource: ReactionDataSource
 ) : PostRepository {
     private val authorId = "user-1feIeEN3rMh4ZY7YpQKxDfnKGvi2"
 
@@ -63,10 +66,43 @@ class PostRepositoryImpl @Inject constructor(
     }
 
     override suspend fun getRandomPostList(count: Int): Result<List<PostContentModel>> {
-        val posts = postDataSource.getRandomPost(authorId, count).getOrElse {
-            throw IOException("게시물 불러오기 실패")
+        return runCatching {
+            val mockAuthorId = "mock"
+
+            val posts = postDataSource.getRandomPost(mockAuthorId, count).getOrElse {
+                throw IOException("게시물 불러오기 실패")
+            }
+            val (tags, reactions) = coroutineScope {
+                val tagsDeferred = async {
+                    posts.map {
+                        tagDataSource.getPostTag(it.postId).getOrElse {
+                            throw IOException("태그 불러오기 실패")
+                        }
+                    }
+                }
+                val reactionsDeferred = async {
+                    posts.map {
+                        reactionDataSource.getReactionByPostId(it.postId).getOrElse {
+                            throw IOException("리액션 불러오기 실패")
+                        }
+                    }
+                }
+
+                tagsDeferred.await() to reactionsDeferred.await()
+            }
+
+            posts.mapIndexed { index, postModel ->
+                PostContentModel(
+                    authorId = postModel.authorId,
+                    imageUrl = postModel.imageUrl,
+                    registerAt = postModel.registerAt,
+                    description = postModel.description,
+                    tags = tags[index].map { it.tagName },
+                    isFollower = false,
+                    reactions = reactions[index]
+                )
+            }
         }
-        return Result.failure(IOException("게시물 불러오기 실패"))
     }
 
     private suspend fun updateImageUrl(postId: String, fileUri: Uri) {
