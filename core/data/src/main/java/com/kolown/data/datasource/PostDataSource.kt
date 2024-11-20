a03b9499fff2db2e55be71958fe949965a1a5f44
Inwoo <lys91892@gmail.com>
2024-11-20 23:33:36 +0900

    Feat : Post 불러오기 DataSource 구현 - #37
    Co-authored-by: bong-pal <etgt777@naver.com>
    Co-authored-by: HahyunKang <ganghahyun06@gmail.com>

--- core/data/src/main/java/com/kolown/data/datasource/PostDataSource.kt
+++ core/data/src/main/java/com/kolown/data/datasource/PostDataSource.kt
@@ -1,8 +1,11 @@
 package com.kolown.data.datasource
 
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.kolown.data.remote.PostDto
+import com.kolown.data.remote.toPostModel
+import com.kolown.model.PostModel
 import kotlinx.coroutines.tasks.await
 import java.time.LocalDateTime
 import javax.inject.Inject
@@ -10,10 +13,12 @@ import javax.inject.Inject
 interface PostDataSource {
     suspend fun uploadPost(authorId: String, description: String): Result<String>
     suspend fun updateImageUrl(documentId: String, imageUrl: String)
+    suspend fun getRandomPost(uid: String, count: Int): Result<List<Any>>
 }
 
 class PostDataSourceImpl @Inject constructor() : PostDataSource {
     private val postCollection = Firebase.firestore.collection("post")
+    private val randomType = listOf("A", "B", "C", "D", "E").random()
 
     override suspend fun uploadPost(
         authorId: String,
@@ -34,5 +39,27 @@ class PostDataSourceImpl @Inject constructor() : PostDataSource {
 
     override suspend fun updateImageUrl(documentId: String, imageUrl: String) {
         postCollection.document(documentId).update("imageUrl", imageUrl)
+    }
+
+    override suspend fun getRandomPost(uid: String, count: Int): Result<List<PostModel>> {
+        val randomValue = (0..Long.MAX_VALUE).random()
+
+        return kotlin.runCatching {
+            val fetchPosts: suspend (Long) -> List<PostModel> = { key ->
+                postCollection
+                    .whereNotEqualTo("authorId", uid)
+                    .whereGreaterThan("random$randomType", key)
+                    .orderBy("random$randomType", Query.Direction.ASCENDING)
+                    .limit(count.toLong())
+                    .get()
+                    .addOnFailureListener {
+                    }
+                    .await()
+                    .map { it.toObject(PostDto::class.java).toPostModel(randomType) }
+            }
+            val result = fetchPosts(randomValue)
+
+            if (result.size > count) result else fetchPosts(0)
+        }
     }
 }
--- core/model/src/main/java/com/kolown/model/Reactions.kt
+++ core/model/src/main/java/com/kolown/model/Reactions.kt
@@ -1,5 +1,11 @@
 package com.kolown.model
 
-enum class Reactions{
-    LOVE,SURPRISE,SMILE,STAR,THUMB,HEART
+import com.kolown.model.Reactions.entries
+
+enum class Reactions(val value: Int) {
+    LOVE(0), SURPRISE(1), SMILE(2), STAR(3), THUMB(4), HEART(5);
+}
+
+fun Int.toReactions(): Reactions {
+    return entries.first { it.value == this }
 }
