0131d9fd8ac559b13703c3f7fb9feaae6588df59
bong-pal <etgt777@naver.com>
2024-11-22 00:09:59 +0900

    Feat: auth repository

--- core/data/src/main/java/com/kolown/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/kolown/data/di/RepositoryModule.kt
@@ -1,9 +1,15 @@
 package com.kolown.data.di
 
+<<<<<<< HEAD
 import com.kolown.data.datasource.FakeGalleryDataSource
 import com.kolown.data.datasource.FakePostDataSource
 import com.kolown.data.datasource.GalleryDataSource
 import com.kolown.data.repository.FakeTagRepository
+=======
+import com.kolown.data.repository.AuthRepository
+import com.kolown.data.repository.AuthRepositoryImpl
+import com.kolown.data.repository.GalleryRepository
+>>>>>>> c581db5 (Feat: auth repository)
 import com.kolown.data.repository.GalleryRepositoryImpl
 import com.kolown.data.repository.GalleryRepository
 import com.kolown.data.repository.PostRepository
@@ -25,4 +31,13 @@ abstract class RepositoryModule {
     @Singleton
     abstract fun provideFakePostRepository(postRepositoryImpl: PostRepositoryImpl): PostRepository
 
+    @Binds
+    abstract fun providePostRepository(
+        postRepository: PostRepositoryImpl,
+    ): PostRepository
+
+    @Binds
+    abstract fun provideAuthRepository(
+        authRepository: AuthRepositoryImpl,
+    ): AuthRepository
 }
