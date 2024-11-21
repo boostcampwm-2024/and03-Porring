b5fb5a86f584acfc0be2f963adc1cc2383aaec35
Moony-H <53536205+Moony-H@users.noreply.github.com>
2024-11-22 00:39:01 +0900

    Fix: 파일 정리

--- app-test-camera/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_kolown_app_test_camera_CameraTestApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.kolown.app_test_camera.CameraTestApplication"
-)
-public final class _com_kolown_app_test_camera_CameraTestApplication {
-}
