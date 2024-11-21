aa135c0f31983919fff0feeb00690a41a03ff4ab
bong-pal <etgt777@naver.com>
2024-11-22 00:09:59 +0900

    Feat: auth repository

---
+++ core/data/src/main/java/com/kolown/data/repository/AuthRepository.kt
@@ -0,0 +1,36 @@
+package com.kolown.data.repository
+
+import androidx.credentials.Credential
+import androidx.credentials.CustomCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.kolown.data.datasource.AuthDataSource
+import javax.inject.Inject
+import javax.inject.Named
+
+interface AuthRepository {
+    suspend fun signInWithCredential(credential: Credential): Result<Unit>
+    fun checkUserLoggedIn(): Boolean
+}
+
+class AuthRepositoryImpl @Inject constructor(
+    @Named("google") private val googleAuthDataSource: AuthDataSource,
+) : AuthRepository {
+    override suspend fun signInWithCredential(credential: Credential): Result<Unit> {
+        return kotlin.runCatching {
+            when (credential) {
+                is CustomCredential -> handleCustomCredential(credential)
+            }
+        }
+    }
+
+    private suspend fun handleCustomCredential(credential: CustomCredential) {
+        if (credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+            googleAuthDataSource.signInWithCredential(credential)
+        }
+    }
+
+    override fun checkUserLoggedIn(): Boolean {
+        return googleAuthDataSource.checkUserLoggedIn()
+    }
+}
+
