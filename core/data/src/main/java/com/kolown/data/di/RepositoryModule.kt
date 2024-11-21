6dd7aab2d2e9393917bd73da9d8e1b9585b18946
bong-pal <etgt777@naver.com>
2024-11-22 04:17:36 +0900

    Fix: di conflict - rebase후 충돌 해결 - repository di모듈 재설정 - 사라진 DI어노테이션 추가

--- core/data/src/main/java/com/kolown/data/di/RepositoryModule.kt
+++ core/data/src/main/java/com/kolown/data/di/RepositoryModule.kt
@@ -10,17 +10,11 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
-
-    @Binds
-    @Singleton
-    abstract fun provideFakePostRepository(postRepositoryImpl: PostRepositoryImpl): PostRepository
-
     @Binds
     abstract fun providePostRepository(
         postRepository: PostRepositoryImpl,
