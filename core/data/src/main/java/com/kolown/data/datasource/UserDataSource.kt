bf185f4be2df040ff86e1b2af56bd19b88d3b96c
bong-pal <etgt777@naver.com>
2024-11-22 00:04:18 +0900

    Feat: user data source

---
+++ core/data/src/main/java/com/kolown/data/datasource/UserDataSource.kt
@@ -0,0 +1,19 @@
+package com.kolown.data.datasource
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import javax.inject.Inject
+
+interface UserDataSource {
+    suspend fun createUserData(userDto: UserDto): Result<Unit>
+}
+
+class UserDataSourceImpl @Inject constructor() : UserDataSource {
+    private val userCollection by lazy { Firebase.firestore.collection("user") }
+
+    override suspend fun createUserData(userDto: UserDto): Result<Unit> {
+        return kotlin.runCatching {
+            userCollection.document(userDto.userId).set(userDto)
+        }
+    }
+}
