f47d5cdf0117976b9b49f35264b2b299e5adff8c
Inwoo <lys91892@gmail.com>
2024-11-21 16:01:07 +0900

    Feat : FirebaseImpl remove @Inject constructor - #37

--- core/data/src/main/java/com/kolown/data/service/FirebaseService.kt
+++ core/data/src/main/java/com/kolown/data/service/FirebaseService.kt
@@ -5,14 +5,13 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storage
-import javax.inject.Inject
 
 interface FirebaseService {
     fun getCollection(collectionName: String): CollectionReference
     fun getStorage(): FirebaseStorage
 }
 
-class FirebaseServiceImpl @Inject constructor(
+class FirebaseServiceImpl (
     private val firebase: Firebase
 ) : FirebaseService {
     override fun getCollection(collectionName: String): CollectionReference {
