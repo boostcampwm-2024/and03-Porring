dfacb61c72bf033469da779917320250d96a6ef6
bong-pal <etgt777@naver.com>
2024-11-22 19:13:09 +0900

    Refactor: extract follow screen component - 로그인 유도 컨텐츠, 팔로우 컨텐츠 컴포넌트 분리

--- feature/follower/src/main/java/com/kolown/follower/FollowerScreen.kt
+++ feature/follower/src/main/java/com/kolown/follower/FollowerScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -14,10 +15,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
+import com.kolown.follower.component.FollowContent
+import com.kolown.follower.component.RestrictedLoginContent
 
 @Composable
 internal fun FollowerRoute(
@@ -43,40 +47,11 @@ private fun FollowerScreen(
         .padding(padding)
 
     if (isLoggedIn) {
-        Box(
-            modifier = modifier,
-            contentAlignment = Alignment.Center
-        ) {
-
-        }
+        FollowContent()
     } else {
-        Column(
+        RestrictedLoginContent(
+            navigateToLogin = navigateToLogin,
             modifier = modifier
-                .padding(top = 60.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            AsyncImage(
-                modifier = Modifier.size(120.dp),
-                model = R.drawable.ic_porring_symbol,
-                contentDescription = null
-            )
-            Spacer(Modifier.size(24.dp))
-            Text(
-                text = stringResource(R.string.string_use_after_login),
-                style = MaterialTheme.typography.labelLarge
-            )
-            TextButton(
-                onClick = { navigateToLogin() },
-            ) {
-                Text(text = stringResource(R.string.string_move_to_login), color = Color.Blue)
-            }
-
-        }
+        )
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun PreviewFollowerScreen() {
-    FollowerScreen(false)
 }
