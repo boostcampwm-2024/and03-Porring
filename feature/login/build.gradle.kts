9ea547c99a0759284ff8934c215ee895f67a4be7
bong-pal <etgt777@naver.com>
2024-11-22 11:05:28 +0900

    Chore: change login sdk version controll

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -20,10 +20,10 @@ android {
     }
 
     namespace = "com.kolown.login"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        minSdk = 24
+        minSdk = libs.versions.minSdk.get().toInt()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
