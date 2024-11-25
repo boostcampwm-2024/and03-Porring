9d69b2582ff668d95ad9191e38dd986b66d94d61
bong-pal <etgt777@naver.com>
2024-11-25 10:47:48 +0900

    UI: change padding follow screen

--- feature/follower/src/main/java/com/kolown/follower/FollowerScreen.kt
+++ feature/follower/src/main/java/com/kolown/follower/FollowerScreen.kt
@@ -1,25 +1,10 @@
 package com.kolown.follower
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.kolown.follower.component.FollowContent
 import com.kolown.follower.component.RestrictedLoginContent
 
@@ -51,7 +36,6 @@ private fun FollowerScreen(
     } else {
         RestrictedLoginContent(
             navigateToLogin = navigateToLogin,
-            modifier = modifier
         )
     }
 }
