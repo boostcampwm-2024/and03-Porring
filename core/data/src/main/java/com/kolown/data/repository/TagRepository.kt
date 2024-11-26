ee02e452b562d1a6d3f421d52fb73dce0ccdaa85
HahyunKang <ganghahyeon06@gmail.com>
2024-11-26 22:37:21 +0900

    Feat: 검색 결과 가져오기

--- core/data/src/main/java/com/kolown/data/repository/TagRepository.kt
+++ core/data/src/main/java/com/kolown/data/repository/TagRepository.kt
@@ -3,20 +3,17 @@ package com.kolown.data.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import com.kolown.data.datasource.fake.FakeTagDataSource
 import com.kolown.data.datasource.paging.TagPagingSource
 import com.kolown.data.datasource.remote.TagDataSource
-import com.kolown.data.mock.MockDataProvider
 import com.kolown.model.Tag
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
 interface TagRepository {
     suspend fun getTagBySearch(search: String): Flow<PagingData<Tag>>
 }
 
-class TagRepositoryImpl(private val tagDataSource: TagDataSource) : TagRepository {
+class TagRepositoryImpl  @Inject constructor(private val tagDataSource: TagDataSource) : TagRepository {
 
     override suspend fun getTagBySearch(search: String): Flow<PagingData<Tag>> {
         return Pager(
