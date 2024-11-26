ff9e166b3ae98b58112c36bcc01303811f30426e
bong-pal <etgt777@naver.com>
2024-11-26 22:34:16 +0900

    Refactor: extract component - login스크린 컴포넌트 분리 - 버튼들을 group으로 처리할수 있도록 변경 - 배경 추가

--- feature/login/src/main/java/com/kolown/login/LoginScreen.kt
+++ feature/login/src/main/java/com/kolown/login/LoginScreen.kt
@@ -1,28 +1,27 @@
 package com.kolown.login
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -33,6 +32,12 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.repeatOnLifecycle
+import com.kolown.login.component.ButtonWithIcon
+import com.kolown.login.component.LoginButtonGroup
+import com.kolown.login.component.LoginTopAppBar
+import com.kolown.login.component.LogoItem
+import com.kolown.login.util.LoginButton
+import com.kolown.login.util.LoginButton.*
 import com.kolown.login.util.LoginPlatform
 import com.kolown.login.util.getCredential
 import kotlinx.coroutines.CoroutineScope
@@ -61,7 +66,6 @@ fun LoginRoute(
         }
     }
 
-    Spacer(modifier = Modifier.height(50.dp))
     LoginScreen(
         onClickGoogleLogin = {
             CoroutineScope(Dispatchers.Main).launch {
@@ -69,7 +73,9 @@ fun LoginRoute(
                     loginViewModel.handleSignIn(it)
                 }
             }
-        }, popBackStack = popBackStack, padding = padding
+        },
+        popBackStack = popBackStack,
+        padding = padding
     )
 }
 
@@ -79,71 +85,54 @@ fun LoginScreen(
     popBackStack: () -> Unit = {},
     padding: PaddingValues = PaddingValues(),
 ) {
-    Column(
+    Box(
         modifier = Modifier.fillMaxSize().padding(padding),
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth().height(52.dp).padding(end = 16.dp),
-            horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            IconButton(onClick = { popBackStack() }) {
-                Icon(
-                    imageVector = Icons.Default.Close,
-                    contentDescription = stringResource(R.string.string_close_button)
-                )
-            }
-        }
-        Spacer(modifier = Modifier.height(200.dp))
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Icon(
-                painter = painterResource(R.drawable.logo_icon),
-                contentDescription = null,
-                tint = Color.Unspecified
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Icon(
-                painter = painterResource(R.drawable.logo_porring),
-                contentDescription = null,
-                tint = Color.Unspecified
-            )
-        }
-        Spacer(modifier = Modifier.height(20.dp))
-        Text(text = stringResource(R.string.app_sub_title), fontSize = 12.sp)
-        Spacer(modifier = Modifier.height(40.dp))
-        Text(text = stringResource(R.string.login_singup), fontSize = 14.sp, color = Color.Gray)
-        Spacer(modifier = Modifier.height(20.dp))
-        ButtonWithIcon(
-            icon = R.drawable.logo_google,
-            text = stringResource(R.string.start_with_google),
-            onClick = onClickGoogleLogin
+        Image(
+            contentScale = ContentScale.Crop,
+            painter = painterResource(R.drawable.bg_login),
+            contentDescription = null
+        )
+
+        LoginContent(
+            onClickGoogleLogin = onClickGoogleLogin
+        )
+
+        LoginTopAppBar(
+            popBackStack = popBackStack
         )
     }
 }
 
 @Composable
-fun ButtonWithIcon(
-    icon: Int,
-    text: String,
-    onClick: () -> Unit = {},
+fun LoginContent(
+    onClickGoogleLogin: () -> Unit = {},
+    modifier: Modifier = Modifier,
 ) {
-    Button(
-        onClick = onClick,
-        modifier = Modifier.width(width = 252.dp),
-        shape = RoundedCornerShape(2.dp),
-        elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 2.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = Color.White, contentColor = Color.Gray
-        )
+    Column(
+        modifier = modifier.fillMaxSize().padding(top = 100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Icon(painter = painterResource(icon), contentDescription = null, tint = Color.Unspecified)
-        Spacer(modifier = Modifier.width(5.dp))
-        Text(text = text, fontSize = 14.sp)
+        LogoItem()
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(text = stringResource(R.string.login_singup), fontSize = 14.sp, color = Color.Gray)
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        LoginButtonGroup (
+            PainterIconButton(
+                icon = R.drawable.logo_google,
+                text = stringResource(R.string.start_with_google),
+                onClick = onClickGoogleLogin
+            ),
+            VectorIconButton(
+                icon = Icons.Default.Email,
+                text = "이메일로 로그인",
+                onClick = onClickGoogleLogin
+            )
+        )
     }
 }
 
---
+++ feature/login/src/main/java/com/kolown/login/component/LogoItem.kt
@@ -0,0 +1,45 @@
+package com.kolown.login.component
+
+import android.graphics.Paint.Align
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kolown.login.R
+
+@Composable
+fun LogoItem() {
+    Column(
+        modifier = Modifier.fillMaxWidth().wrapContentHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Icon(
+            modifier = Modifier.fillMaxWidth(0.7f),
+            painter = painterResource(R.drawable.logo_porring),
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
+
+        Text(text = stringResource(R.string.app_sub_title), fontSize = 12.sp)
+    }
+}
+
+@Preview (showBackground = true)
+@Composable
+private fun PreviewLogoItem() {
+    LogoItem()
+}
