c488c4b6b3ca01dd7ae925887095e47ad2ed5c26
Inwoo <lys91892@gmail.com>
2024-11-26 14:16:47 +0900

    Feat : Upload enable - #28

--- feature/upload/src/main/java/com/kolown/upload/UploadViewModel.kt
+++ feature/upload/src/main/java/com/kolown/upload/UploadViewModel.kt
@@ -10,7 +10,10 @@ import com.kolown.data.repository.PostRepository
 import com.kolown.model.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -29,6 +32,16 @@ class UploadViewModel @Inject constructor(
     val uiState = _uiState.asStateFlow()
 
     private val webPUri = MutableStateFlow<Uri?>(null)
+
+    val uploadEnable = combine(
+        _description, _categoryItems, webPUri
+    ) { description, categoryItems, webPUri ->
+        description.isNotEmpty() && categoryItems.none { it.isBlank() } && webPUri != null
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = false
+    )
 
     fun changeDescription(description: String) {
         _description.value = description
