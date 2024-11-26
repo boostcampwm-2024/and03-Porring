bc167f8a8c61aa710fc7745c239739188c24081a
HahyunKang <ganghahyeon06@gmail.com>
2024-11-27 01:26:42 +0900

    Feat: 팔로워 스크린 연결

--- core/data/src/main/java/com/kolown/data/datasource/paging/FollowerGalleryThumbnailPagingSource.kt
+++ core/data/src/main/java/com/kolown/data/datasource/paging/FollowerGalleryThumbnailPagingSource.kt
@@ -1,35 +1,105 @@
 package com.kolown.data.datasource.paging
 
+import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.kolown.data.datasource.fake.FollowerDataSource
+import com.kolown.data.datasource.remote.AuthDataSource
+import com.kolown.data.datasource.remote.FollowDataSource
+import com.kolown.data.datasource.remote.PostDataSource
+import com.kolown.data.datasource.remote.ReactionDataSource
+import com.kolown.data.datasource.remote.TagDataSource
 import com.kolown.model.FollowerThumbnail
+import com.kolown.model.PostContentModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Named
 
-class FollowerGalleryThumbnailPagingDataSource(
-    private val followerDataSource: FollowerDataSource,
-) : PagingSource<Int, FollowerThumbnail>() {
+class FollowerGalleryThumbnailPagingDataSource @Inject constructor(
+    private val followerDataSource: FollowDataSource,
+    private val postDataSource: PostDataSource,
+    private val tagDataSource: TagDataSource,
+    private val reactionDataSource: ReactionDataSource,
+    @Named("google") private val googleAuthDataSource: AuthDataSource,
+) : PagingSource<String, FollowerThumbnail>() {
 
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, FollowerThumbnail> {
+    override suspend fun load(params: LoadParams<String>): LoadResult<String, FollowerThumbnail> {
         return try {
-            val page = params.key ?: 0
-            val result = followerDataSource.getFollowerThumbnailAlbum(page)
-            val body = result.getOrElse {
-                return LoadResult.Error(it)
+            val currentUserId = googleAuthDataSource.getUserId()
+            val key = params.key
+            Log.e("팔로워-current",currentUserId)
+
+            val followers = followerDataSource.getFollowerList(
+                userId = currentUserId,
+                key = key,
+                perPage = 1
+            ).getOrElse { throw Exception("팔로워 불러오기 실패") }
+
+            followers.forEach {
+                Log.e("팔로워",it.followerName)
             }
+
+            val thumbnails = followers.map {
+                val posts = postDataSource.getUserPost(uid= it.followerId, perPage = params.loadSize.toLong()).getOrElse { throw Exception("게시물 불러오기 실패") }
+                val (tags, reactions) = coroutineScope {
+                    val tagsDeferred = async {
+                        posts.map {
+                            async {
+                                tagDataSource.getPostTag(it.postId).getOrElse {
+                                    throw IOException("태그 불러오기 실패")
+                                }
+                            }
+                        }.awaitAll()
+                    }
+                    val reactionsDeferred = async {
+                        posts.map {
+                            async {
+                                reactionDataSource.getReactionByPostId(it.postId).getOrElse {
+                                    throw IOException("리액션 불러오기 실패")
+                                }
+                            }
+                        }.awaitAll()
+                    }
+
+                    tagsDeferred.await() to reactionsDeferred.await()
+                }
+                val data = posts.mapIndexed { index, postModel ->
+                    PostContentModel(
+                        postId = postModel.postId,
+                        authorId = postModel.authorId,
+                        imageUrl = postModel.imageUrl,
+                        registerAt = postModel.registerAt,
+                        description = postModel.description,
+                        tags = tags[index].map { it.tagName },
+                        isFollower = false,
+                        reactions = reactions[index].mapNotNull { it.reaction },
+                        myReaction = reactions[index].find { it.userId == currentUserId }?.reaction
+                    )
+                }
+
+                FollowerThumbnail(
+                    id = it.followerId,
+                    followerName = it.followerName,
+                    posts = data
+                )
+            }
+
             LoadResult.Page(
-                data = body,
-                prevKey = if (page == 0) null else page - 1,
-                nextKey = if (body.isEmpty()) null else page + 1
+                data = thumbnails,
+                prevKey = if (key == null) null  else followers.firstOrNull()?.followerId,
+                nextKey = if (followers.isEmpty()) null else followers.lastOrNull()?.followerId
             )
         } catch (e: Exception) {
             LoadResult.Error(e)
         }
     }
 
-    override fun getRefreshKey(state: PagingState<Int, FollowerThumbnail>): Int? {
-        return state.anchorPosition?.let { position ->
-            val closestPage = state.closestPageToPosition(position)
-            closestPage?.prevKey?.plus(1) ?: closestPage?.nextKey?.minus(1)
+    override fun getRefreshKey(state: PagingState<String, FollowerThumbnail>): String? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey
         }
     }
 }
--- feature/follower/src/main/java/com/kolown/follower/FollowViewModel.kt
+++ feature/follower/src/main/java/com/kolown/follower/FollowViewModel.kt
@@ -2,14 +2,42 @@ package com.kolown.follower
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.kolown.data.repository.FollowerRepository
+import com.kolown.data.repository.FollowRepository
+import com.kolown.model.FollowerThumbnail
+import com.kolown.model.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FollowerViewModel @Inject constructor(
-    private val followerRepository: FollowerRepository
+    private val followerRepository: FollowRepository
 ) : ViewModel() {
-    val galleryFlow = followerRepository.getFollowerDataSourcePagingFlow(1).cachedIn(viewModelScope)
+
+    private val _uiState = MutableStateFlow<UiState<Flow<PagingData<FollowerThumbnail>>>>(UiState.Loading)
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        getItem()
+    }
+
+    private fun getItem() {
+        viewModelScope.launch {
+            _uiState.value = UiState.Loading
+            try {
+                val follows =
+                    followerRepository.getFollowerDataSourcePagingFlow().cachedIn(viewModelScope)
+                _uiState.value = UiState.Success(follows)
+            } catch (e:Exception) {
+                _uiState.value = UiState.Failure(e)
+            }
+        }
+    }
+
+
 }
