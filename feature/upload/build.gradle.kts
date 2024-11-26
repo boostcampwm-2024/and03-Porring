e9c6b935c1103766bf6342306854ac56adf300db
Inwoo <lys91892@gmail.com>
2024-11-26 14:16:28 +0900

    Chore : implementation designsystem - #28

--- feature/upload/build.gradle.kts
+++ feature/upload/build.gradle.kts
@@ -56,4 +56,5 @@ dependencies {
     implementation(projects.core.navigation)
     implementation(projects.core.model)
     implementation(projects.core.data)
+    implementation(projects.core.designsystem)
 }
