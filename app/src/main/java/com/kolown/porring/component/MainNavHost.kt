c46c75faced35811df436164597701e6bd836b3a
Moony-H <53536205+Moony-H@users.noreply.github.com>
2024-11-27 21:49:17 +0900

    Feat: Permission 화면 수정

--- app/src/main/java/com/kolown/porring/component/MainNavHost.kt
+++ app/src/main/java/com/kolown/porring/component/MainNavHost.kt
@@ -72,7 +72,7 @@ internal fun MainNavHost(
                         UploadModel("", "", emptyList())
                     )
                 },
-                padding = padding
+                padding = padding,
                 popBackStack = navigator::popBackStack
             )
 
