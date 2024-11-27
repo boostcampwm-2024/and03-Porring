096fd55afc0f9881ec8facc9711185de6b21cdee
Inwoo <lys91892@gmail.com>
2024-11-27 11:19:06 +0900

    Fix : Rebase error fix - #28

--- core/data/src/main/java/com/kolown/data/di/RepositoryModuleProvides.kt
+++ core/data/src/main/java/com/kolown/data/di/RepositoryModuleProvides.kt
@@ -1,22 +1,13 @@
 package com.kolown.data.di
 
-import android.content.Context
-import com.kolown.data.datasource.FakeGalleryDataSource
-import com.kolown.data.datasource.GalleryDataSource
-import com.kolown.data.repository.FakeTagRepository
 import com.kolown.data.datasource.fake.FakeGalleryDataSource
 import com.kolown.data.repository.GalleryRepository
 import com.kolown.data.repository.GalleryRepositoryImpl
-import com.kolown.data.repository.ImageCacheRepository
-import com.kolown.data.repository.ImageCacheRepositoryImpl
-import com.kolown.data.repository.TagRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-
 
 @Module
 @InstallIn(SingletonComponent::class)
