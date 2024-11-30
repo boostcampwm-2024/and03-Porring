67e7423c058868a109d52e8ef2f09ace393e3e58
bong-pal <etgt777@naver.com>
2024-12-01 02:31:40 +0900

    Fix: ã set login screen single top

--- feature/login/src/main/java/com/kolown/login/navigation/LoginNavigation.kt
+++ feature/login/src/main/java/com/kolown/login/navigation/LoginNavigation.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navOptions
 import com.kolown.login.LoginRoute
 import com.kolown.navigation.Route
 
 fun NavController.navigateLogin() {
-    navigate(Route.Login)
+    navigate(
+        route = Route.Login,
+        navOptions = navOptions { launchSingleTop = true }
+    )
 }
 
 fun NavGraphBuilder.loginNavGraph(
