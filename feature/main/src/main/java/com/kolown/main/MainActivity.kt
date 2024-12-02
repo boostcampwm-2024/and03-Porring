32256e07fc45237ee120dc343e3bc6862f8dcce7
Inwoo <lys91892@gmail.com>
2024-12-02 15:53:12 +0900

    Feat : 캡쳐 방지 구현 - #73

--- feature/main/src/main/java/com/kolown/main/MainActivity.kt
+++ feature/main/src/main/java/com/kolown/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kolown.main
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -37,5 +38,6 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
     }
 }
