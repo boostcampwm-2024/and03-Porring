f15a59ceaa7b44515cae78c7ccb19f45bbbce293
HahyunKang <ganghahyeon06@gmail.com>
2024-12-04 14:43:09 +0900

    Fix: 홈화면에서 더블 탭할 때 시스템바 사라지는 현상 해결

--- feature/home/src/main/java/com/kolown/home/HomeScreen.kt
+++ feature/home/src/main/java/com/kolown/home/HomeScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
