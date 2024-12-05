73b11c84f64d94535cf00ae2627cd6de8d089268
Moony-H <53536205+Moony-H@users.noreply.github.com>
2024-12-05 22:59:22 +0900

    Feat: NotAvailableVersionScreen created

---
+++ feature/main/src/main/java/com/kolown/main/component/NotAvailableVersionScreen.kt
@@ -0,0 +1,52 @@
+package com.kolown.main.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import com.kolown.common.R
+import com.kolown.designsystem.ui.theme.Primary
+
+@Composable
+fun NotAvailableVersionScreen(
+    onExitAppButtonClicked: () -> Unit = {},
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(R.drawable.ic_porring_symbol),
+            modifier = Modifier.size(120.dp),
+            contentDescription = null,
+            tint = Primary
+        )
+
+        Text(
+            text = stringResource(R.string.description_not_available_version),
+            style = MaterialTheme.typography.labelLarge
+        )
+        TextButton(
+            onClick = onExitAppButtonClicked,
+        ) {
+            Text(
+                text = stringResource(R.string.button_exit_app),
+                color = Primary
+            )
+        }
+    }
+}
