23c82774a64f5bc392f82160124eed2a71ed3554
Moony-H <53536205+Moony-H@users.noreply.github.com>
2024-12-05 15:16:11 +0900

    File: Hilt library implement

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -41,6 +43,9 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    //hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
 
     //datastore
     implementation(libs.androidx.datastore)
