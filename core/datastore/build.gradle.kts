a448314b3d90a92aeccb0dd919c6064cd30caa90
Moony-H <53536205+Moony-H@users.noreply.github.com>
2024-12-05 15:11:02 +0900

    File: DataStore library implement

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -40,4 +40,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    //datastore
+    implementation(libs.androidx.datastore)
+    implementation(libs.androidx.datastore.preferences)
 }
