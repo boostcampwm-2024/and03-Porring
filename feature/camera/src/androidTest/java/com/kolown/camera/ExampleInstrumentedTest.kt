06db84995ad855d5eafba94e22900a7651929354
Moony-H <53536205+Moony-H@users.noreply.github.com>
2024-12-12 18:40:54 +0900

    Fix: nested code delete

--- feature/camera/src/androidTest/java/com/kolown/camera/ExampleInstrumentedTest.kt
+++ feature/camera/src/androidTest/java/com/kolown/camera/ExampleInstrumentedTest.kt
@@ -1,7 +1,14 @@
 package com.kolown.camera
 
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,9 +23,14 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
+    fun useAppContext()= runTest{
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        Log.e("test","테스트 시작")
+        delay(5000)
+        Log.e("test","테스트 끝")
+        advanceUntilIdle()
         assertEquals("com.kolown.camera.test", appContext.packageName)
+
     }
 }
